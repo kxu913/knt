e8d896f17021e4e5d9220f529204636a61334a60
kxu913 <kxu913@gmail.com>
2014-01-16 23:28:30 +0800

    remove test

--- ny6design/ny6design_core/src/main/java/com/ny6design/constant/Messages.java
+++ ny6design/ny6design_core/src/main/java/com/ny6design/constant/Messages.java
@@ -7,7 +7,5 @@ package com.ny6design.constant;
 public class Messages {
 
 	public static final String ERROR_CATEGORY_DELETE = "Cannot delete the category because it has subcategories.";
-	
-	public static final String test="test";
 
 }

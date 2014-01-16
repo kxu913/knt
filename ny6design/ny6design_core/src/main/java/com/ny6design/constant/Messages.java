867b21a3ff25b877da3187aa1b534d948ecf5eb7
neo <1797301460@qq.com>
2014-01-16 23:18:30 +0800

    neo's test

--- ny6design/ny6design_core/src/main/java/com/ny6design/constant/Messages.java
+++ ny6design/ny6design_core/src/main/java/com/ny6design/constant/Messages.java
@@ -7,5 +7,7 @@ package com.ny6design.constant;
 public class Messages {
 
 	public static final String ERROR_CATEGORY_DELETE = "Cannot delete the category because it has subcategories.";
+	
+	public static final String test="test";
 
 }

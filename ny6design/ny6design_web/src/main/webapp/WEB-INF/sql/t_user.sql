c15ab51d4028ae15cfa92eabe48fb226ccf8f9a1
kxu913 <kxu913@gmail.com>
2014-01-25 23:38:29 +0800

    修改用户表

--- ny6design/ny6design_web/src/main/webapp/WEB-INF/sql/t_user.sql
+++ ny6design/ny6design_web/src/main/webapp/WEB-INF/sql/t_user.sql
@@ -1,15 +1,3 @@
-﻿# MySQL-Front 5.0  (Build 1.78)
-
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE */;
-/*!40101 SET SQL_MODE='STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES */;
-/*!40103 SET SQL_NOTES='ON' */;
-
-
-# Host: localhost:3307    Database: ny6design
-# ------------------------------------------------------
-# Server version 5.6.7-rc
-
 USE `ny6design`;
 
 #
@@ -30,8 +18,7 @@ CREATE TABLE `t_user` (
   `taxId` decimal(10,3) DEFAULT NULL,
   `fromId` int(11) NOT NULL DEFAULT '0',
   `factive` varchar(1) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `isAdmin` tinyint(3) NOT NULL DEFAULT '0',
+  `updateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`userId`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;

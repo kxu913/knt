5e924db829ef59374577e9b70e778aaaa72d8021
Kevin Xu <kevin.xu@morningstar.com>
2014-02-16 15:15:47 +0800

    修改 t_address 和 t_user_address 表

---
+++ ny6design/ny6design_web/src/main/resources/sql/t_address.sql
@@ -0,0 +1,17 @@
+﻿USE `ny6design`;
+
+#
+# Table structure for table t_address
+#
+
+DROP TABLE IF EXISTS `t_address`;
+CREATE TABLE `t_address` (
+  `addressId` int(11) NOT NULL AUTO_INCREMENT,
+  `countryCode` varchar(10) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `stateCode` varchar(10) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `town` varchar(40) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `zipcode` varchar(20) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `address` varchar(60) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `fdefault` smallint(6) NOT NULL,
+  PRIMARY KEY (`addressId`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
---
+++ ny6design/ny6design_web/src/main/resources/sql/t_user_address.sql
@@ -0,0 +1,13 @@
+﻿USE `ny6design`;
+
+#
+# Table structure for table t_user_address
+#
+
+DROP TABLE IF EXISTS `t_user_address`;
+CREATE TABLE `t_user_address` (
+  `id` INT NOT NULL,
+  `userId` INT NOT NULL,
+  `addressId` INT NULL COMMENT 'mapping of user and address',
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;

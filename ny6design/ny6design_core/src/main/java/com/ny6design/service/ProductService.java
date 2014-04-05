86ebe552c88a50d93d42aa51e6cd7ddc62d32b51
kxu913 <kxu913@gmail.com>
2014-04-06 01:48:24 +0800

    需要完善一个通过PID和数量来计算总额的方法

--- ny6design/ny6design_core/src/main/java/com/ny6design/service/ProductService.java
+++ ny6design/ny6design_core/src/main/java/com/ny6design/service/ProductService.java
@@ -1,6 +1,6 @@
 package com.ny6design.service;
 
-
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
@@ -23,189 +23,193 @@ import com.ny6design.model.ProductPrice;
 
 @Service
 public class ProductService {
-    
+
 	Logger logger = Logger.getLogger(ProductService.class);
-	
-    @Autowired
-    ProductMapper productMapper;
-    @Autowired
-    ProductPriceMapper productPriceMapper;
-    @Autowired
-    ProductImageMapper productImageMapper;
-    @Autowired
-    ProductDescriptionMapper productDescriptionMapper;
-    @Autowired
-    Product2CategoryMapper productCategoryMapper;
-    
-/*    public Product getProduct(long productId){
-        return productMapper.getProduct(productId);
-    }*/
-    
-    @Transactional
-    public void insertProduct(Product product){
-    	productMapper.insertProduct(product);
-    }
-    
-    private void copyAndInsertProduct(Product existProduct){
-    	
-    	if(existProduct!=null){
-    		//get the exist product info
-    		ProductDescription pd = existProduct.getProductDesc();
-        	List<ProductPrice> ppList = existProduct.getProductPriceList();
-        	List<ProductImage> piList = existProduct.getProductImageList();
-        	List<Product2CategoryKey> pcList = existProduct.getProductCategoryList();
-        	
-			
-			
-    		//insert product info
-    		existProduct.setProductId(null);
-    		existProduct.setStatus(1);//set Disabled
-    		productMapper.insertProduct(existProduct);
-    		
-    		int newpId = existProduct.getProductId(); 
-    		//copy description info
-    		if(pd!=null){
-    			pd.setProductId(newpId);
-    			productMapper.insertProductDesc(pd);
-    		}
-    		
-    		//copy price data
-    		if(ppList!=null && ppList.size()>0){
-    			for(ProductPrice pp : ppList){
-    				pp.setProductId(newpId);
-    				productMapper.insertProductPrice(pp);
-    			}
-    		}
-    		
-    		if(pcList!=null && pcList.size()>0){
-    			for(Product2CategoryKey pc : pcList){
-    				if(pc==null) continue;
-    				pc.setProductId(newpId);
-    				productMapper.insertProductCategory(pc);
-    			}
-    		}
-    		
-    		//TODO add productImage
-    		if(piList!=null && piList.size()>0){
-    			
-    		}
-    	}
-    } 
-    
-    public void copyPorduct(String productIds){
-    	if(StringUtils.contains(productIds+",", ",")){
-    		String[] productIdArray = productIds.split(",");
-    		if(productIdArray.length>0){
-    			for(String productId: productIdArray){
-    				// get, then remove productid, save product, get pid, and save refer tables;
-    				Product p= productMapper.getProductDetail(Integer.valueOf(productId));
-    				copyAndInsertProduct(p);
-    			}
-    		}
-    	}
-    }
-    
-    public void setPorductWithIndex(String productIds, Integer asIndex){
-    	if(StringUtils.contains(productIds+",", ",")){
-    		String[] productIdArray = productIds.split(",");
-    		if(productIdArray.length>0){
-    			for(String productId: productIdArray){
-    				Product prodcut = productMapper.getProductDetail(Integer.parseInt(productId));
-    				if(prodcut!=null && asIndex!=null && !(asIndex==prodcut.getIndex())){
-    					prodcut.setIndex(asIndex);
-    					productMapper.updateProductById(prodcut);
-    				}
-    			}
-    		}
-    	}
-    }
-    
-    public List<List<Product>> getProductList4Front(int categoryId, int numInLine){
-    	List<Product> productList = productMapper.getProductsByCategory(categoryId);
-    	List<List<Product>> resultList = Lists.newArrayList();
-		if(productList!=null && productList.size()>0){
+
+	@Autowired
+	ProductMapper productMapper;
+	@Autowired
+	ProductPriceMapper productPriceMapper;
+	@Autowired
+	ProductImageMapper productImageMapper;
+	@Autowired
+	ProductDescriptionMapper productDescriptionMapper;
+	@Autowired
+	Product2CategoryMapper productCategoryMapper;
+
+	/*
+	 * public Product getProduct(long productId){ return productMapper.getProduct(productId); }
+	 */
+
+	@Transactional
+	public void insertProduct(Product product) {
+		productMapper.insertProduct(product);
+	}
+
+	private void copyAndInsertProduct(Product existProduct) {
+
+		if (existProduct != null) {
+			// get the exist product info
+			ProductDescription pd = existProduct.getProductDesc();
+			List<ProductPrice> ppList = existProduct.getProductPriceList();
+			List<ProductImage> piList = existProduct.getProductImageList();
+			List<Product2CategoryKey> pcList = existProduct.getProductCategoryList();
+
+			// insert product info
+			existProduct.setProductId(null);
+			existProduct.setStatus(1);// set Disabled
+			productMapper.insertProduct(existProduct);
+
+			int newpId = existProduct.getProductId();
+			// copy description info
+			if (pd != null) {
+				pd.setProductId(newpId);
+				productMapper.insertProductDesc(pd);
+			}
+
+			// copy price data
+			if (ppList != null && ppList.size() > 0) {
+				for (ProductPrice pp : ppList) {
+					pp.setProductId(newpId);
+					productMapper.insertProductPrice(pp);
+				}
+			}
+
+			if (pcList != null && pcList.size() > 0) {
+				for (Product2CategoryKey pc : pcList) {
+					if (pc == null)
+						continue;
+					pc.setProductId(newpId);
+					productMapper.insertProductCategory(pc);
+				}
+			}
+
+			// TODO add productImage
+			if (piList != null && piList.size() > 0) {
+
+			}
+		}
+	}
+
+	public void copyPorduct(String productIds) {
+		if (StringUtils.contains(productIds + ",", ",")) {
+			String[] productIdArray = productIds.split(",");
+			if (productIdArray.length > 0) {
+				for (String productId : productIdArray) {
+					// get, then remove productid, save product, get pid, and save refer tables;
+					Product p = productMapper.getProductDetail(Integer.valueOf(productId));
+					copyAndInsertProduct(p);
+				}
+			}
+		}
+	}
+
+	public void setPorductWithIndex(String productIds, Integer asIndex) {
+		if (StringUtils.contains(productIds + ",", ",")) {
+			String[] productIdArray = productIds.split(",");
+			if (productIdArray.length > 0) {
+				for (String productId : productIdArray) {
+					Product prodcut = productMapper.getProductDetail(Integer.parseInt(productId));
+					if (prodcut != null && asIndex != null && !(asIndex == prodcut.getIndex())) {
+						prodcut.setIndex(asIndex);
+						productMapper.updateProductById(prodcut);
+					}
+				}
+			}
+		}
+	}
+
+	public List<List<Product>> getProductList4Front(int categoryId, int numInLine) {
+		List<Product> productList = productMapper.getProductsByCategory(categoryId);
+		List<List<Product>> resultList = Lists.newArrayList();
+		if (productList != null && productList.size() > 0) {
 			int size = productList.size();
 			List<Product> temp = null;
-			for(int i=0; i<size; i++){
-				if(i==0){
+			for (int i = 0; i < size; i++) {
+				if (i == 0) {
 					temp = Lists.newArrayList();
 				}
 				temp.add(productList.get(i));
-				if(  (i+1)%numInLine == 0 ){
+				if ((i + 1) % numInLine == 0) {
 					resultList.add(temp);
 					temp = Lists.newArrayList();
 				}
-				if((i+1)==size){
-					/*if(temp.size()<numInLine && temp.size()>0){
-						while(temp.size()<numInLine){
-							temp.add(null);
-						}
-					}*/
+				if ((i + 1) == size) {
+					/*
+					 * if(temp.size()<numInLine && temp.size()>0){ while(temp.size()<numInLine){ temp.add(null); } }
+					 */
 					resultList.add(temp);
 				}
 			}
 		}
 		return resultList;
-    }
-    
-    
-    public List<List<Product>> getIndexProductList4Front(int numInLine){
-    	List<Product> productList = productMapper.getIndexProducts4FE();
-    	List<List<Product>> resultList = Lists.newArrayList();
-		if(productList!=null && productList.size()>0){
+	}
+
+	public List<List<Product>> getIndexProductList4Front(int numInLine) {
+		List<Product> productList = productMapper.getIndexProducts4FE();
+		List<List<Product>> resultList = Lists.newArrayList();
+		if (productList != null && productList.size() > 0) {
 			int size = productList.size();
 			List<Product> temp = null;
-			for(int i=0; i<size; i++){
-				if(i==0){
+			for (int i = 0; i < size; i++) {
+				if (i == 0) {
 					temp = Lists.newArrayList();
 				}
 				temp.add(productList.get(i));
-				if(  (i+1)%numInLine == 0 ){
+				if ((i + 1) % numInLine == 0) {
 					resultList.add(temp);
 					temp = Lists.newArrayList();
 				}
-				if((i+1)==size){
-					/*if(temp.size()<numInLine && temp.size()>0){
-						while(temp.size()<numInLine){
-							temp.add(null);
-						}
-					}*/
+				if ((i + 1) == size) {
+					/*
+					 * if(temp.size()<numInLine && temp.size()>0){ while(temp.size()<numInLine){ temp.add(null); } }
+					 */
 					resultList.add(temp);
 				}
 			}
 		}
 		return resultList;
-    }
-    
-    public void deleteProduct(String productIds){
-    	if(StringUtils.contains(productIds+",", ",")){
-    		String[] productIdArray = productIds.split(",");
-    		if(productIdArray.length>0){
-    			for(String productId: productIdArray){
-    				productMapper.deleteProductById(Integer.valueOf(productId));
-    				productMapper.deleteProductDescByProductId(Integer.valueOf(productId));
-    				productMapper.deleteProductImpageByProductId(Integer.valueOf(productId));
-    				productMapper.deleteProductPriceByProductId(Integer.valueOf(productId));
-    			}
-    		}
-    	}
-    }
-    
-    public Product getProductInfo(Integer productId){
-    	return productMapper.getProductDetail(productId);
-    }
-    
-/*    public List<Product> getProductPageList(long categoryId, int start, int end){
-    	return productMapper.getProductPageList(categoryId, start, end);
-    }
-
-	public List<Product> getRecommendProductList() {
-		return productMapper.getRecommendProductList();
 	}
 
-	public int getProductCount(long categoryId) {
-		
-		return productMapper.getProductCount(categoryId);
+	public void deleteProduct(String productIds) {
+		if (StringUtils.contains(productIds + ",", ",")) {
+			String[] productIdArray = productIds.split(",");
+			if (productIdArray.length > 0) {
+				for (String productId : productIdArray) {
+					productMapper.deleteProductById(Integer.valueOf(productId));
+					productMapper.deleteProductDescByProductId(Integer.valueOf(productId));
+					productMapper.deleteProductImpageByProductId(Integer.valueOf(productId));
+					productMapper.deleteProductPriceByProductId(Integer.valueOf(productId));
+				}
+			}
+		}
 	}
-*/
+
+	public Product getProductInfo(Integer productId) {
+		return productMapper.getProductDetail(productId);
+	}
+
+	/**
+	 * Get total cost by amount
+	 * 
+	 * @param pId productId
+	 * @param amount 
+	 * @return total cost
+	 */
+
+	public BigDecimal getTotalCost(int pId, int amount) {
+		// TODO
+		return this.getProductInfo(pId).getPrice().multiply(new BigDecimal(amount));
+	}
+
+	/*
+	 * public List<Product> getProductPageList(long categoryId, int start, int end){ return productMapper.getProductPageList(categoryId,
+	 * start, end); }
+	 * 
+	 * public List<Product> getRecommendProductList() { return productMapper.getRecommendProductList(); }
+	 * 
+	 * public int getProductCount(long categoryId) {
+	 * 
+	 * return productMapper.getProductCount(categoryId); }
+	 */
 }

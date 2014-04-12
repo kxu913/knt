f0cc686942ce34afd68514f50d9742ee14c96ff3
Kevin Xu <kevin.xu@morningstar.com>
2014-04-12 16:42:58 +0800

    add fileds support

--- ny6design/ny6design_core/src/main/java/com/ny6design/model/CartDetail.java
+++ ny6design/ny6design_core/src/main/java/com/ny6design/model/CartDetail.java
@@ -4,6 +4,12 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.ny6design.service.calc.Calculator;
+import com.ny6design.service.calc.DiscountCalculator;
+import com.ny6design.service.calc.ExtraCalculator;
+import com.ny6design.service.calc.OrderCalculator;
+import com.ny6design.service.calc.ShipCalculator;
+
 /**
  * display whole information of cart
  * 
@@ -13,8 +19,14 @@ import java.util.List;
 public class CartDetail {
 	private ShoppingCart cart;
 	private List<OrderDetail> orders;
-
+	private ShipMethod shipMethod;
+	private PayMethod payMethod;
+	private Discount discount;
 	private BigDecimal subtotal;
+	private BigDecimal total;
+	private String notice;
+	// whether initialize the orders
+	private boolean hasInited = false;
 
 	public ShoppingCart getCart() {
 		return cart;
@@ -38,6 +50,70 @@ public class CartDetail {
 
 	public void setSubtotal(BigDecimal subtotal) {
 		this.subtotal = subtotal;
+	}
+
+	public ShipMethod getShipMethod() {
+		return shipMethod;
+	}
+
+	public void setShipMethod(ShipMethod shipMethod) {
+		this.shipMethod = shipMethod;
+	}
+
+	public BigDecimal getTotal() {
+		return total;
+	}
+
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
+
+	public BigDecimal calcSubTotal() {
+		Calculator orderCalculator = new OrderCalculator(this.getOrders());
+		return orderCalculator.calc();
+	}
+
+	public Discount getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(Discount discount) {
+		this.discount = discount;
+	}
+
+	public PayMethod getPayMethod() {
+		return payMethod;
+	}
+
+	public void setPayMethod(PayMethod payMethod) {
+		this.payMethod = payMethod;
+	}
+
+	public String getNotice() {
+		return notice;
+	}
+
+	public void setNotice(String notice) {
+		this.notice = notice;
+	}
+
+	public boolean isHasInited() {
+		return hasInited;
+	}
+
+	public void setHasInited(boolean hasInited) {
+		this.hasInited = hasInited;
+	}
+
+	public BigDecimal calcTotal() {
+		Calculator orderCalculator = new OrderCalculator(this.getOrders());
+		// for test
+		discount = new Discount();
+		discount.setDiscount(BigDecimal.ONE);
+		// end
+		ExtraCalculator discoutCalculator = new DiscountCalculator(orderCalculator, discount);
+		ExtraCalculator shipExtraCalculator = new ShipCalculator(discoutCalculator, shipMethod);
+		return shipExtraCalculator.calc();
 	}
 
 }

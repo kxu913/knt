2e30df93e257adb7e2f9a9052aa326b4690934d9
Kevin Xu <kevin.xu@morningstar.com>
2014-04-15 17:04:28 +0800

    add order summary

---
+++ ny6design/ny6design_core/src/main/java/com/ny6design/model/OrderSummary.java
@@ -0,0 +1,115 @@
+package com.ny6design.model;
+
+import java.util.Date;
+
+public class OrderSummary {
+    private Integer id;
+
+    private Integer shipmethodid;
+
+    private Integer paymethodid;
+
+    private Integer discountid;
+
+    private Double subtotal;
+
+    private Double total;
+
+    private String notice;
+
+    private Integer userid;
+
+    private Integer status;
+
+    private Date createtime;
+
+    private Date lastupdatetime;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getShipmethodid() {
+        return shipmethodid;
+    }
+
+    public void setShipmethodid(Integer shipmethodid) {
+        this.shipmethodid = shipmethodid;
+    }
+
+    public Integer getPaymethodid() {
+        return paymethodid;
+    }
+
+    public void setPaymethodid(Integer paymethodid) {
+        this.paymethodid = paymethodid;
+    }
+
+    public Integer getDiscountid() {
+        return discountid;
+    }
+
+    public void setDiscountid(Integer discountid) {
+        this.discountid = discountid;
+    }
+
+    public Double getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(Double subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public Double getTotal() {
+        return total;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
+    }
+
+    public String getNotice() {
+        return notice;
+    }
+
+    public void setNotice(String notice) {
+        this.notice = notice == null ? null : notice.trim();
+    }
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public Date getLastupdatetime() {
+        return lastupdatetime;
+    }
+
+    public void setLastupdatetime(Date lastupdatetime) {
+        this.lastupdatetime = lastupdatetime;
+    }
+}
---
+++ ny6design/ny6design_core/src/main/java/com/ny6design/model/OrderSummaryMapping.java
@@ -0,0 +1,33 @@
+package com.ny6design.model;
+
+public class OrderSummaryMapping {
+    private Integer id;
+
+    private Integer ordersummaryid;
+
+    private Integer orderid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getOrdersummaryid() {
+        return ordersummaryid;
+    }
+
+    public void setOrdersummaryid(Integer ordersummaryid) {
+        this.ordersummaryid = ordersummaryid;
+    }
+
+    public Integer getOrderid() {
+        return orderid;
+    }
+
+    public void setOrderid(Integer orderid) {
+        this.orderid = orderid;
+    }
+}
